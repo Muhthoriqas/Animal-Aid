bacc029b65abc0e2de06ad0cedb02b40a9e75fb6
Thoriq As <72277295+Muhthoriqas@users.noreply.github.com>
2023-03-07 19:40:29 +0800

    Update

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let navbar = document.querySelector(".header .navbar");
 document.querySelector("#menu-btn").onclick = () => {
   navbar.classList.toggle("active");
 };
+
 window.onscroll = () => {
   navbar.classList.remove("active");
 };
